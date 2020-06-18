dbfb13036088c84aa0c9b3315419ed0221d305ea
Archil Karchava <archil1997@gmail.com>
2020-06-18 13:23:52 +0500

    Custom 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,13 +1,21 @@
+import { Link } from "gatsby"
 import React from "react"
-import Layout from "../components/Layout"
 import SEO from "../components/SEO"
 
 const NotFoundPage: React.FC = () => (
-  <Layout>
+  <>
     <SEO title="404: Страница не найдена" />
-    <h1>Такой страницы не существует.</h1>
-    <p>Вы перешли на страницу которой не существует.</p>
-  </Layout>
+    <div className="flex flex-col items-center justify-center h-screen p-5">
+      <div>
+        <h1 className="mb-5 text-4xl font-bold">
+          Такой страницы не существует.
+        </h1>
+        <Link to="/" className="text-xl text-blue-600 underline">
+          Вернуться на главную
+        </Link>
+      </div>
+    </div>
+  </>
 )
 
 export default NotFoundPage
