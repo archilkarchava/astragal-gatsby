bb92c84b7a4622fc2ef73efff0a312b7c5eabfb0
Archil Karchava <archil1997@gmail.com>
2020-06-18 12:58:18 +0500

    Added gatsby-plugin-zeit-now

--- gatsby-config.js
+++ gatsby-config.js
@@ -126,6 +126,7 @@ module.exports = {
       resolve: `gatsby-plugin-robots-txt`,
     },
     `gatsby-plugin-loadable-components-ssr`,
+    `gatsby-plugin-zeit-now`
     // "gatsby-plugin-webpack-bundle-analyser-v2",
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
