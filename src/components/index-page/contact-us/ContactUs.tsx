9e8899a8ea1a019938881313e92d3b8f7b7c78e6
Archil Karchava <archil1997@gmail.com>
2020-06-23 22:21:17 +0500

    Added slide-up animation to contact us section

--- src/components/index-page/contact-us/ContactUs.tsx
+++ src/components/index-page/contact-us/ContactUs.tsx
@@ -5,10 +5,20 @@ import ContactInfo from "./ContactInfo"
 const ContactUs: React.FC = () => {
   return (
     <section className="flex flex-col justify-between w-full overflow-hidden text-gray-900 bg-white md:flex-row">
-      <div className="w-full md:w-1/2">
+      <div
+        data-sal="slide-up"
+        data-sal-delay="300"
+        data-sal-easing="ease"
+        className="w-full md:w-1/2"
+      >
         <ContactInfo />
       </div>
-      <div className="w-full bg-gray-100 md:w-1/2">
+      <div
+        data-sal="slide-up"
+        data-sal-delay="500"
+        data-sal-easing="ease"
+        className="w-full bg-gray-100 md:w-1/2"
+      >
         <ContactForm />
       </div>
     </section>
