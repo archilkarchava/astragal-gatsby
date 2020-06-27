0510e332600479a9b626a043285c460d154fce22
Archil Karchava <archil1997@gmail.com>
2020-06-27 09:54:04 +0500

    Throttling scroll listener to improve performance

--- src/components/index-page/map/Map.tsx
+++ src/components/index-page/map/Map.tsx
@@ -1,4 +1,5 @@
 import loadable from "@loadable/component"
+import throttle from "lodash/throttle"
 import React from "react"
 import { useShowMap } from "../../../hooks/contextHooks"
 
@@ -7,24 +8,21 @@ const YandexMap = loadable(() => import("./YandexMap"))
 const Map: React.FC = () => {
   const sectionRef = React.useRef<HTMLDivElement>()
   const [isShowMap, setIsShowMap] = useShowMap()
-  const handleShowMap = React.useCallback(() => {
-    if (
-      window.pageYOffset > sectionRef.current.getBoundingClientRect().height
-    ) {
-      setIsShowMap(true)
-    }
-  }, [sectionRef, setIsShowMap])
 
   React.useEffect(() => {
-    handleShowMap()
-  }, [handleShowMap])
-
-  React.useEffect(() => {
+    const handleShowMap = throttle(() => {
+      if (
+        window.pageYOffset > sectionRef.current.getBoundingClientRect().height
+      ) {
+        setIsShowMap(true)
+        window.removeEventListener("scroll", handleShowMap)
+      }
+    }, 5)
     window.addEventListener("scroll", handleShowMap)
     return () => {
       window.removeEventListener("scroll", handleShowMap)
     }
-  }, [handleShowMap])
+  }, [])
 
   return (
     <section ref={sectionRef} className="w-full h-72 md:h-149">
